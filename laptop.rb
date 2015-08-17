f13f10a588315bf85095874afac179dca5b83f93
Toby Clemson <tobyclemson@gmail.com>
2015-08-17 13:38:12 +0100

    Add support for brews list file.

--- laptop.rb
+++ laptop.rb
@@ -1,3 +1,12 @@
-dep 'laptop' do
+parse = -> (line) {
+  name, provide_name = line.strip.split(':')
+  [name, -> (type) { dep "#{name}.#{type}" do provides provide_name if provide end }]
+}
 
+brews = File.readlines(File.expand_path '../brews.lst', __FILE__).map &parse
+
+brews.each { |name, dep_of_type| dep_of_type['managed'] }
+
+dep 'laptop' do
+  brews.each { |name, _| requires "#{name}.managed" }
 end

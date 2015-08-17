8f60f8560d956e2624265daa860bc4191ac76782
Toby Clemson <tobyclemson@gmail.com>
2015-08-17 13:52:30 +0100

    Fix ruby errors.

--- laptop.rb
+++ laptop.rb
@@ -1,9 +1,9 @@
-parse = -> (line) {
+parse = ->(line) {
   name, provide_name = line.strip.split(':')
-  [name, -> (type) { dep "#{name}.#{type}" do provides provide_name if provide end }]
+  [name, ->(type) { dep "#{name}.#{type}" do provides provide_name if provide end }]
 }
 
-brews = File.readlines(File.expand_path '../brews.lst', __FILE__).map &parse
+brews = File.readlines(File.expand_path '../brews.lst', __FILE__).map(&parse)
 
 brews.each { |name, dep_of_type| dep_of_type['managed'] }
 

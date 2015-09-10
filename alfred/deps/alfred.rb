921c69cda7b2426220aad5216e17382c0a2fb3e8
Toby Clemson <tobyclemson@gmail.com>
2015-09-10 14:35:09 +0100

    Synchronise plist state in plist template.

--- alfred/deps/alfred.rb
+++ alfred/deps/alfred.rb
@@ -2,6 +2,18 @@ dep 'alfred license file.plist' do
   requires 'alfred.cask'
   
   target "~/Library/Application Support/Alfred 2/license.plist"
+  entries [
+    {
+      :path => ":'email'",
+      :type => 'string',
+      :value => 'tobyclemson@gmail.com'
+    },
+    {
+      :path => ":'code'",
+      :type => 'string',
+      :value => 'code'
+    }
+  ]
 end
 
 dep 'alfred' do
--- plist/templates/plist.rb
+++ plist/templates/plist.rb
@@ -1,9 +1,51 @@
 meta :plist do
   accepts_value_for :target
+  accepts_list_for :entries
 
   template {
     setup {
       requires 'plist exists'.with(target: target)
+    }
+
+    def plistbuddy
+      '/usr/libexec/PlistBuddy'
+    end
+
+    def add_entry path, type, target
+      "#{plistbuddy} -c \"Add #{path} #{type}\" \"#{File.expand_path(target)}\""
+    end
+
+    def set_entry path, value, target
+      "#{plistbuddy} -c \"Set #{path} #{value}\" \"#{File.expand_path(target)}\""
+    end
+
+    def hash_entry path, target
+      "#{plistbuddy} -c \"Print #{path}\" \"#{File.expand_path(target)}\" | md5"
+    end
+
+    def hash_value value
+      "echo '#{value}' | md5"
+    end
+
+    def check_exists path, target
+      "#{plistbuddy} -c \"Print #{path}\" \"#{File.expand_path(target)}\" 2>&1 | grep -vq 'Does Not Exist'"
+    end
+
+    def check_equal path, value, target
+      "bash -c \"[ \\\"$(#{hash_entry(path, target)})\\\" == \\\"$(#{hash_value(value)})\\\" ]\""
+    end
+
+    met? {
+      entries.all? do |entry|
+        shell?(check_exists(entry[:path], target)) && shell?(check_equal(entry[:path], entry[:value], target))
+      end
+    }
+
+    meet {
+      entries.each do |entry|
+        shell(add_entry(entry[:path], entry[:type], target)) unless shell?(check_exists(entry[:path], target))
+        shell(set_entry(entry[:path], entry[:value], target)) unless shell?(check_equal(entry[:path], entry[:value], target))
+      end
     }
   }
 end

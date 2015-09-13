7463aed89b8f227821acb6ab56e1b69eac042f9f
Toby Clemson <tobyclemson@gmail.com>
2015-09-13 17:31:16 +0100

    Add Dropbox login dep.

--- dropbox/deps/dropbox.rb
+++ dropbox/deps/dropbox.rb
@@ -4,7 +4,18 @@ dep 'dropbox.loginitem' do
   path shell("readlink '#{'~/Applications/Dropbox.app'.p.cleanpath}'")
 end
 
+dep 'dropbox credentials' do
+  met? {
+    '~/.dropbox/info.json'.p.exist?
+  }
+
+  meet {
+    shell("read \"Log in to Dropbox. Hit return when complete.\"")
+  }
+end
+
 dep 'dropbox' do
   requires 'dropbox.cask'
+  requires 'dropbox credentials'
   requires 'dropbox.loginitem'
 end

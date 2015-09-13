71e02ca3dbb50d009c88a34368b9606ac42dc04f
Toby Clemson <tobyclemson@gmail.com>
2015-09-13 13:35:52 +0100

    Disable plugins automatically in IntelliJ configuration.

--- intellij/deps/intellij.rb
+++ intellij/deps/intellij.rb
@@ -10,7 +10,15 @@ dep 'intellij license.file' do
   target '~/Library/Preferences/IntelliJIdea14/idea14.key'
 end
 
+dep 'intellij disabled plugins.file' do
+  requires 'intellij-idea.cask'
+
+  source "#{__FILE__.p.parent}/../files/disabled_plugins.txt"
+  target '~/Library/Preferences/IntelliJIdea14/disabled_plugins.txt'
+end
+
 dep 'intellij' do
   requires 'intellij-idea.cask'
   requires 'intellij license.file'
+  requires 'intellij disabled plugins.file'
 end

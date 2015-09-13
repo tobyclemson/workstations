07b98ea2e7d4f8dd9d39f9d0d89f02293220ac72
Toby Clemson <tobyclemson@gmail.com>
2015-09-13 16:40:18 +0100

    Start SizeUp on system start up.

--- sizeup/deps/sizeup.rb
+++ sizeup/deps/sizeup.rb
@@ -10,7 +10,19 @@ dep 'sizeup license.file' do
   target '~/Library/Application Support/SizeUp/SizeUp.sizeuplicense'
 end
 
+dep 'sizeup start at login', :for => :osx do
+  met? {
+    shell("osascript -e 'tell application \"System Events\" to get every login item whose name is \"SizeUp\"'") =~ "SizeUp"
+  }
+
+  meet {
+    shell("osascript -e 'tell application \"System Events\" to make login item at end with properties {path:\"/opt/homebrew-cask/Caskroom/sizeup/latest/SizeUp.app\", hidden:false, name:\"SizeUp\"}'
+")
+  }
+end
+
 dep 'sizeup' do
   requires 'sizeup.cask'
   requires 'sizeup license.file'
+  requires 'sizeup start at login'
 end

f47ce0a4f1f84f0cc365c0fb488fce275ed1e607
Toby Clemson <tobyclemson@gmail.com>
2015-09-20 15:56:34 +0100

    Unbox if list of one item.

--- defaults/templates/defaults.rb
+++ defaults/templates/defaults.rb
@@ -1,36 +1,44 @@
 meta "defaults" do
   accepts_value_for :domain
   accepts_value_for :key
-  accepts_value_for :value
+  accepts_list_for :value
 
   template {
+    def resolved_value
+      if value.length == 1
+        value[0]
+      else
+        value
+      end
+    end
+
     def read_value
-      case value
+      case resolved_value
       when Array
-        ["(\n", value.map { |e| "    #{e}" }.join(",\n"), "\n", ")\n"].flatten.join
+        ["(\n", resolved_value.map { |e| "    #{e}" }.join(",\n"), "\n", ")\n"].flatten.join
       when true
         "1"
       when false
         "0"
       else
-        value.to_s
+        resolved_value.to_s
       end
     end
 
     def type
-      return "array" if value.is_a? Array
-      return "bool"  if [true, false].include? value
-      return "int"   if value.is_a? Integer
-      return "float" if value.is_a? Float
+      return "array" if resolved_value.is_a? Array
+      return "bool"  if [true, false].include? resolved_value
+      return "int"   if resolved_value.is_a? Integer
+      return "float" if resolved_value.is_a? Float
       return "string"
     end
 
     def write_value
-      case value
+      case resolved_value
       when Array
-        "'(#{value.join(',')})'"
+        "'(#{resolved_value.join(',')})'"
       else
-        value.to_s.include?(" ") ? "'#{value.to_s}'" : value.to_s
+        resolved_value.to_s.include?(" ") ? "'#{resolved_value.to_s}'" : resolved_value.to_s
       end
     end
 

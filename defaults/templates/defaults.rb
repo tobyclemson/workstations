811624467eb4c3d49177d8f5c54107bf7699d430
Toby Clemson <tobyclemson@gmail.com>
2015-09-20 16:38:21 +0100

    Fix array comparison in defaults template.

--- defaults/templates/defaults.rb
+++ defaults/templates/defaults.rb
@@ -6,8 +6,8 @@ meta "defaults" do
 
   template {
     def read_value
-      if values
-        return ["(\n", value.map { |e| "    #{e}" }.join(",\n"), "\n", ")\n"].flatten.join
+      if !values.empty?
+        return ["(\n", values.map { |e| "    \"#{e}\"" }.join(",\n"), "\n", ")"].flatten.join
       end
 
       case value
@@ -21,7 +21,7 @@ meta "defaults" do
     end
 
     def type
-      return "array" if values.is_a? Array
+      return "array" if !values.empty?
       return "bool"  if [true, false].include? value
       return "int"   if value.is_a? Integer
       return "float" if value.is_a? Float
@@ -29,14 +29,17 @@ meta "defaults" do
     end
 
     def write_value
-      if values
-        return "'(#{values.join(',')})'"
+      if !values.empty?
+        quoted_values = values.map { |e| "'#{e}'" }
+        return "#{quoted_values.join(' ')}"
       end
 
       value.to_s.include?(" ") ? "'#{value.to_s}'" : value.to_s
     end
 
-    met? { `defaults read #{domain} #{key}`.strip == read_value }
+    met? {
+      `defaults read #{domain} #{key}`.strip == read_value
+    }
     meet { log_shell "Setting #{domain} #{key} to #{write_value}", "defaults write #{domain} #{key} -#{type} #{write_value}" }
   }
 end

da36816576940d7c96eda6700ee5616db1594afa
Toby Clemson <tobyclemson@gmail.com>
2018-07-17 17:06:05 +0100

    Fix path to dropbox for login item.

--- dropbox/deps/dropbox.rb
+++ dropbox/deps/dropbox.rb
@@ -1,7 +1,7 @@
 dep 'dropbox.loginitem' do
   name "Dropbox"
   hidden false
-  path shell("readlink '#{'~/Applications/Dropbox.app'.p.cleanpath}'")
+  path "/Applications/Dropbox.app".p.cleanpath
 end
 
 dep 'dropbox credentials' do

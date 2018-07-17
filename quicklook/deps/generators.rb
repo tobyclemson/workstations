5e632e9d4bb202d26217711d81e3e7604bf72f8e
Toby Clemson <tobyclemson@gmail.com>
2018-07-17 16:11:12 +0100

    Remove betterzipql since there is no longer a cask for it.

--- quicklook/deps/generators.rb
+++ quicklook/deps/generators.rb
@@ -3,7 +3,6 @@ dep 'qlstephen.cask', :for => :osx
 dep 'qlmarkdown.cask', :for => :osx
 dep 'quicklook-json.cask', :for => :osx
 dep 'quicklook-csv.cask', :for => :osx
-dep 'betterzipql.cask', :for => :osx
 dep 'qlimagesize.cask', :for => :osx
 dep 'suspicious-package.cask', :for => :osx
 
@@ -13,7 +12,6 @@ dep 'quicklook plugins' do
   requires 'qlmarkdown.cask'
   requires 'quicklook-json.cask'
   requires 'quicklook-csv.cask'
-  requires 'betterzipql.cask'
   requires 'qlimagesize.cask'
   requires 'suspicious-package.cask'
 

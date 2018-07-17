25020525129968e8a71dd8995cd1843af1ddf421
Toby Clemson <tobyclemson@gmail.com>
2018-07-17 10:59:06 +0100

    Disable more.

--- laptop.rb
+++ laptop.rb
@@ -1,38 +1,38 @@
-parse = ->(line) {
-  name, parameters = line.strip.split(',')
-  name = eval(name)
-  parameters = parameters ? eval("{#{parameters}}") : {}
-
-  require 'pp'
-  pp [name, parameters]
-
-  {name: name, parameters: parameters}
-}
-
-homebrew_dep_of_type = ->(type) {
-  ->(definition) {
-    name = definition[:name]
-    parameters = definition[:parameters]
-
-    require 'pp'
-    pp ["#{name}.#{type}", parameters]
-
-    dep "#{name}.#{type}" do
-      requires parameters[:requires] if parameters[:requires]
-      opts parameters[:options] if parameters[:options]
-    end
-  }
-}
-
-brew_dep = homebrew_dep_of_type.('brew')
-cask_dep = homebrew_dep_of_type.('cask')
-
-# taps = File.readlines(File.expand_path '../taps.lst', __FILE__).map(&parse)
-brews = File.readlines(File.expand_path '../brews.lst', __FILE__).map(&parse)
-casks = File.readlines(File.expand_path '../casks.lst', __FILE__).map(&parse)
-
-brews.each(&brew_dep)
-casks.each(&cask_dep)
+# parse = ->(line) {
+#   name, parameters = line.strip.split(',')
+#   name = eval(name)
+#   parameters = parameters ? eval("{#{parameters}}") : {}
+#
+#   require 'pp'
+#   pp [name, parameters]
+#
+#   {name: name, parameters: parameters}
+# }
+#
+# homebrew_dep_of_type = ->(type) {
+#   ->(definition) {
+#     name = definition[:name]
+#     parameters = definition[:parameters]
+#
+#     require 'pp'
+#     pp ["#{name}.#{type}", parameters]
+#
+#     dep "#{name}.#{type}" do
+#       requires parameters[:requires] if parameters[:requires]
+#       opts parameters[:options] if parameters[:options]
+#     end
+#   }
+# }
+#
+# brew_dep = homebrew_dep_of_type.('brew')
+# cask_dep = homebrew_dep_of_type.('cask')
+#
+# # taps = File.readlines(File.expand_path '../taps.lst', __FILE__).map(&parse)
+# brews = File.readlines(File.expand_path '../brews.lst', __FILE__).map(&parse)
+# casks = File.readlines(File.expand_path '../casks.lst', __FILE__).map(&parse)
+#
+# brews.each(&brew_dep)
+# casks.each(&cask_dep)
 
 dep 'laptop' do
   # taps.each {|tap| requires "homebrew tap".with(tap)}

2183b3889d82b765a55c6edf91775f8b50d87466
Toby Clemson <tobyclemson@gmail.com>
2018-07-21 11:16:51 +0100

    Enable key repeat override for karabiner.

--- karabiner/deps/karabiner.rb
+++ karabiner/deps/karabiner.rb
@@ -1,24 +1,47 @@
+dep 'karabiner preferences.plist' do
+  requires 'karabiner.cask'
+
+  target '~/Library/Preferences/org.pqrs.Karabiner.plist'
+  entries [
+    {
+      :path => ":kIsOverwriteKeyRepeat",
+      :type => 'bool',
+      :value => 'true'
+    }
+  ]
+end
+
 dep 'karabiner repeat wait.karabinersetting' do
+  requires 'karabiner preferences.plist'
+
   setting 'repeat.wait'
   value '10'
 end
 
 dep 'karabiner repeat initial wait.karabinersetting' do
+  requires 'karabiner preferences.plist'
+
   setting 'repeat.initial_wait'
   value '50'
 end
 
 dep 'karabiner remap uk keyboard to sane layout.karabinertoggle' do
+  requires 'karabiner preferences.plist'
+
   setting 'remap.uk_saneuklayout'
   enabled true
 end
 
 dep 'karabiner remap Fn-FN to functional.karabinertoggle' do
+  requires 'karabiner preferences.plist'
+
   setting 'remap.fnF1_F12_to_functional'
   enabled true
 end
 
 dep 'karabiner remap FN to plain FN.karabinertoggle' do
+  requires 'karabiner preferences.plist'
+
   setting 'remap.functional2fn_2008'
   enabled true
 end
@@ -33,6 +56,7 @@ end
 
 dep 'karabiner' do
   requires 'karabiner.cask'
+  requires 'karabiner preferences.plist'
   requires 'karabiner.loginitem'
   requires 'karabiner repeat wait.karabinersetting'
   requires 'karabiner repeat initial wait.karabinersetting'

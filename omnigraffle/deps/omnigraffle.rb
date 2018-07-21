50555559c58eb6fb7c5924bda9bde2bf60e96211
Toby Clemson <tobyclemson@gmail.com>
2018-07-21 10:18:39 +0100

    Update omnigraffle key and fix install location.

--- omnigraffle/deps/omnigraffle.rb
+++ omnigraffle/deps/omnigraffle.rb
@@ -1,45 +1,45 @@
-# dep 'omnigraffle license file.plist' do
-#   requires 'omnigraffle.cask'
-#
-#   def key
-#     GpgHelper.decrypt('omnigraffle/files/key.gpg')
-#   end
-#
-#   def user
-#     shell('whoami')
-#   end
-#
-#   target '~/Library/Containers/com.omnigroup.OmniGraffle6/Data/Library/Application Support/Omni Group/Software Licenses/OmniGraffle-762283.omnilicense'
-#   entries [
-#     {
-#       :path => ":AddedByUser",
-#       :type => 'string',
-#       :value => user
-#     },
-#     {
-#       :path => ":Application",
-#       :type => 'string',
-#       :value => '1000215'
-#     },
-#     {
-#       :path => ":DateAdded",
-#       :type => 'string',
-#       :value => '01/01/2015 00:00 Europe/London'
-#     },
-#     {
-#       :path => ":Owner",
-#       :type => 'string',
-#       :value => 'ThoughtWorks'
-#     },
-#     {
-#       :path => ":Key",
-#       :type => 'string',
-#       :value => key
-#     },
-#   ]
-# end
+dep 'omnigraffle license file.plist' do
+  requires 'omnigraffle.cask'
+
+  def key
+    GpgHelper.decrypt('omnigraffle/files/key.gpg')
+  end
+
+  def user
+    shell('whoami')
+  end
+
+  target '~/Library/Containers/com.omnigroup.OmniGraffle7/Data/Library/Application Support/Omni Group/Software Licenses/OmniGraffle-1144196.omnilicense'
+  entries [
+    {
+      :path => ":AddedByUser",
+      :type => 'string',
+      :value => user
+    },
+    {
+      :path => ":Application",
+      :type => 'string',
+      :value => '1000221'
+    },
+    {
+      :path => ":DateAdded",
+      :type => 'string',
+      :value => '01/01/2018 00:00 Europe/London'
+    },
+    {
+      :path => ":Owner",
+      :type => 'string',
+      :value => 'Toby Clemson'
+    },
+    {
+      :path => ":Key",
+      :type => 'string',
+      :value => key
+    },
+  ]
+end
 
 dep 'omnigraffle' do
   requires 'omnigraffle.cask'
-  # requires 'omnigraffle license file.plist'
+  requires 'omnigraffle license file.plist'
 end

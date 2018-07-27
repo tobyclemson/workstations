03e7272230de35a5f93de19001c3cc0ba746c546
Toby Clemson <tobyclemson@gmail.com>
2018-07-27 19:20:16 +0100

    Only look to depth 1 when locating java installation directory.

--- java/templates/javaversion.rb
+++ java/templates/javaversion.rb
@@ -7,7 +7,7 @@ meta :javaversion do
     }
 
     meet {
-      base_path = shell("find /Library/Java/JavaVirtualMachines -type d -name '*#{version}*'")
+      base_path = shell("find /Library/Java/JavaVirtualMachines -depth 1 -type d -name '*#{version}*'")
       shell("jenv add #{base_path}/Contents/Home")
     }
   }

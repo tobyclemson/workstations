bdc79c3ca4dd1cebf393669bd6875f375c1d8a74
Toby Clemson <tobyclemson@gmail.com>
2019-10-20 12:57:00 +0100

    Fix jenv setup.

--- java/deps/jenv.rb
+++ java/deps/jenv.rb
@@ -2,8 +2,8 @@ dep 'jenv-versions.directory' do
   path '~/.jenv/versions'
 end
 
-dep 'java-1.8.javaversion' do
-  version '1.8'
+dep 'java-8.javaversion' do
+  version '8'
 end
 
 dep 'java-10.javaversion' do
@@ -12,9 +12,9 @@ end
 
 dep 'java' do
   requires 'java.cask'
-  requires 'java8.cask'
+  requires 'adoptopenjdk8.cask'
   requires 'jenv.brew'
   requires 'jenv-versions.directory'
-  requires 'java-1.8.javaversion'
+  requires 'java-8.javaversion'
   requires 'java-10.javaversion'
 end

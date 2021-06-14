8f46fceaffb72c97949eab27bb567f8158caca9a
Toby Clemson <tobyclemson@gmail.com>
2021-06-14 13:42:38 +0100

    Add all taps, brews and casks to Brewfile. Configure git and karabiner.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
-#! /usr/local/bin/env bash
+#! /usr/bin/env bash
 
+USER_NAME=${USER_NAME:-Toby Clemson}
+USER_EMAIL=${USER_EMAIL:-tobyclemson@gmail.com}
+
+# Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tobyclemson/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install all taps, brews, casks
 brew bundle
 
+# Setup git
+if ! git config --global --get user.name | grep -q "$USER_NAME"; then
+  git config --global user.name "$USER_NAME"
+fi
+if ! git config --global --get user.email | grep -q "$USER_EMAIL"; then
+  git config --global user.email "$USER_EMAIL"
+fi
+
+# Setup karabiner
+cp -R ./dotfiles/.config ~

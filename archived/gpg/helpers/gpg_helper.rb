a869f9124af67ac269a6d49747cb8d39212ce92b
Toby Clemson <tobyclemson@gmail.com>
2022-03-06 01:18:29 +0000

    Delete more archived stuff.

--- archived/gpg/helpers/gpg_helper.rb
+++
@@ -1,9 +0,0 @@
-class GpgHelper
-  class << self
-    include Babushka::ShellHelpers
-
-    def decrypt(path)
-      log_shell("Decrypting value from #{path}", "gpg -d #{__FILE__.p.parent.parent.parent}/#{path}")
-    end
-  end
-end
